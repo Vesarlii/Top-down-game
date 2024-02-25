4ac5bbe8b6350db5f5b9538fd6cc69eab2692e29
Pysia <bkubinska111@gmail.com>
2024-02-25 19:01:12 +0100

    add a test version of the map

--- Scene1.js
+++ Scene1.js
@@ -9,6 +9,10 @@ class Scene1 extends Phaser.Scene {
         this.load.spritesheet('playerRight', 'images/spritesheets/right.png', { frameWidth: 86, frameHeight: 62 });
         this.load.spritesheet('playerFront', 'images/spritesheets/front.png', { frameWidth: 86, frameHeight: 62 });
         this.load.spritesheet('playerBack', 'images/spritesheets/back.png', { frameWidth: 86, frameHeight: 62 });
+
+        
+        this.load.image('mapImage', 'images/map/map01.png');
+        this.load.image('itemsImage', 'images/map/items01.png');
     }
 
     create() {
@@ -16,14 +20,22 @@ class Scene1 extends Phaser.Scene {
         const gameWidth = window.innerWidth;
         const gameHeight = window.innerHeight;
 
-        this.physics.world.setBounds(0, 0, gameWidth, gameHeight);
+        
+        this.add.image(0, 0, 'mapImage').setOrigin(0);
 
         this.player = this.physics.add.sprite(gameWidth / 2, gameHeight / 2, 'playerRight');
         this.player.setCollideWorldBounds(true);
 
+        
+        const itemsLayer = this.add.image(0, 0, 'itemsImage').setOrigin(0);
+        
+        
+        this.physics.add.collider(this.player, itemsLayer, (player, item) => {
+            
+        });
+
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        // Dodaj zmienną do śledzenia stanu wciśnięcia klawiszy w poprzedniej klatce
         this.prevKeyState = { left: false, right: false, up: false, down: false };
 
         this.anims.create({
@@ -45,33 +57,28 @@ class Scene1 extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('playerBack', { start: 0, end: 7 }),
             frameRate: 10,
             repeat: -1
-        });
+        }); 
 
-        this.cameras.main.setBackgroundColor('#ffffff'); // Ustaw kolor tła na biały
+        this.cameras.main.setBackgroundColor('#ffffff'); 
     }
 
     update() {
         const speed = 200;
 
-        // Zeruj prędkość gracza na początku aktualizacji
         this.player.setVelocity(0, 0);
 
-        // Ustal, czy któryś z klawiszy był wciśnięty w poprzedniej klatce
         const wasLeftKeyDown = this.prevKeyState.left;
         const wasRightKeyDown = this.prevKeyState.right;
         const wasUpKeyDown = this.prevKeyState.up;
         const wasDownKeyDown = this.prevKeyState.down;
 
-        // Ustal, czy któryś z klawiszy jest wciśnięty w obecnej klatce
         const isLeftKeyDown = this.cursors.left.isDown;
         const isRightKeyDown = this.cursors.right.isDown;
         const isUpKeyDown = this.cursors.up.isDown;
         const isDownKeyDown = this.cursors.down.isDown;
 
-        // Aktualizuj stan wciśnięcia klawiszy w poprzedniej klatce
         this.prevKeyState = { left: isLeftKeyDown, right: isRightKeyDown, up: isUpKeyDown, down: isDownKeyDown };
 
-        // Aktualizuj prędkość gracza na podstawie stanu wciśnięcia klawiszy
         if (isLeftKeyDown) {
             this.player.setVelocityX(-speed);
             this.player.anims.play('right', true);
@@ -90,7 +97,6 @@ class Scene1 extends Phaser.Scene {
             this.player.anims.play('front', true);
         }
 
-        // Zatrzymaj animacje, gdy klawisze zmienią swoje stany
         if (wasLeftKeyDown && !isLeftKeyDown || wasRightKeyDown && !isRightKeyDown) {
             this.player.anims.stop('right');
         }
